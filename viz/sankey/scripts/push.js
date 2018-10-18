89f77fd13f98394b9cdc5f3ed6554c013afb9faf
Yulan Lin <yulan.lin.2014@gmail.com>
2018-10-18 11:45:41 -0700

    edited push script

--- viz/sankey/scripts/push.js
+++ viz/sankey/scripts/push.js
@@ -13,5 +13,5 @@ program
 
 const GCS_BUCKET = program.args[0] === 'true' ? PROD_BUCKET : DEV_BUCKET;
 
-shell.cd('./dist')
+shell.cd('dist')
 shell.exec('gsutil cp -a public-read * ' + GCS_BUCKET);
