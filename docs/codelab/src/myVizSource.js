9ca9a3195ddf032b71eb117f80306cc68f3966a8
mhawksey <m.hawksey@gmail.com>
2018-10-18 14:20:21 +0200

    Updated ti make bars fill more of component window

--- docs/codelab/src/myVizSource.js
+++ docs/codelab/src/myVizSource.js
@@ -1,62 +1,52 @@
-var canvasElement = document.createElement("canvas");
-var ctx = canvasElement.getContext("2d");
+var canvasElement = document.createElement('canvas');
+var ctx = canvasElement.getContext('2d');
 ctx.canvas.width = dscc.getWidth() - 20;
-ctx.canvas.height = dscc.getHeight() - 100;
-canvasElement.id = "myViz";
+ctx.canvas.height = dscc.getHeight() - 40;
+canvasElement.id = 'myViz';
 document.body.appendChild(canvasElement);
 
-var barWidth = 50;
-var barGap = 10;
-var maxBarHeight = 300;
-var canvasPadding = 50;
-
 function drawViz(vizData) {
-  var data = dscc.rowsByConfigId(vizData).DEFAULT;
-  // Place the canvas element on the page.
-  var ctx = canvasElement.getContext("2d");
 
-  // Clear the canvas.
-  ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    var data = dscc.rowsByConfigId(vizData).DEFAULT;
+    // Place the canvas element on the page.
+    var ctx = canvasElement.getContext('2d');
 
-  // 'barMetric' comes from the id defined in myViz.json
-  var rowsTotal = 0;
-  for (let row of data){
-    rowsTotal += row["barMetric"][0];
-  }
+    var barWidth = ctx.canvas.width / (data.length * 2);
+    var maxBarHeight = ctx.canvas.height - 20;
 
-  // parse the style to an object with keys "configId": "value"
-  var parsedStyle = {};
-  for (let section of vizData.config.style) {
-    for (let element of section.elements){
-      parsedStyle[element.id] = element.value;
+    // Clear the canvas.
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    // 'barMetric' comes from the id defined in myViz.json
+    var rowsMax = 0;
+    for (var i = 0; i < data.length; i++) {
+
+        if (data[i]['barMetric'][0]  / maxBarHeight > rowsMax ){
+            rowsMax = data[i]['barMetric'][0] / maxBarHeight;
+        }
     }
-  }
 
-  // Use the Bar Color style element value to set the rectangle color.
-  ctx.fillStyle = parsedStyle.barColor.color;
+    // Use the Bar Color style element value to set the rectangle color.
+    ctx.fillStyle = '#000000' || vizData.config.style[0].elements[0].value.color;
 
+    // Calculate height and draw bars for each row of data.
+    for (var i = 0; i < data.length; i++) {
+        var barHeight = Math.round(
+            -1 * (data[i]['barMetric'][0] / rowsMax)
+        );
+        var barX = (ctx.canvas.width / data.length) * i + barWidth / 2;
+        // Draw bars.
+        ctx.fillRect(barX, maxBarHeight, barWidth, barHeight);
 
-  var textYOffset = 20;
-
-  // Calculate height and draw bars for each row of data.
-  data.forEach(function(row, i){
-    var barHeight = Math.round(
-      -1 * maxBarHeight * (row["barMetric"][0] / rowsTotal)
-    );
-    var barX = (ctx.canvas.width / data.length) * i + barWidth / 2;
-    // Draw bars.
-    ctx.fillRect(barX, maxBarHeight, barWidth, barHeight);
-
-    // Add dimension labels below bars.
-    // 'barDimension' comes from the id defined in myViz.json
-    ctx.fillText(
-      row["barDimension"][0],
-      barX + barWidth / 4,
-      maxBarHeight + textYOffset
-    );
-
-  });
-
+        // Add dimension labels below bars.
+        // 'barDimension' comes from the id defined in myViz.json
+        ctx.fillText(
+            data[i]['barDimension'][0],
+            barX + barWidth / 4,
+            maxBarHeight + 20
+        );
+    }
 }
+
 // Subscribe to Data and Style changes.
 dscc.subscribeToData(drawViz);
