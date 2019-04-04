713838af4cfd2eed380f12258d8bf3d8c65a6016
Yulan Lin <yulan.lin.2014@gmail.com>
2019-04-04 10:51:04 -0700

    first pass at interactions code

---
+++ docs/interactions/scripts/data/localData.js
@@ -0,0 +1,194 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "barDimension": [
+          "Americas"
+        ],
+        "barMetric": [
+          45022
+        ]
+      },
+      {
+        "barDimension": [
+          "Europe"
+        ],
+        "barMetric": [
+          17163
+        ]
+      },
+      {
+        "barDimension": [
+          "Asia"
+        ],
+        "barMetric": [
+          14777
+        ]
+      },
+      {
+        "barDimension": [
+          "Oceania"
+        ],
+        "barMetric": [
+          1408
+        ]
+      },
+      {
+        "barDimension": [
+          "(not set)"
+        ],
+        "barMetric": [
+          1032
+        ]
+      },
+      {
+        "barDimension": [
+          "Africa"
+        ],
+        "barMetric": [
+          661
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "barDimension": [
+      {
+        "id": "qt_13jagch2vb",
+        "name": "Continent",
+        "type": "CONTINENT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "barMetric": [
+      {
+        "id": "qt_hm578bh2vb",
+        "name": "Sessions",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {
+    "barColor": {
+      "value": {
+        "color": "#000000"
+      },
+      "defaultValue": "#000000"
+    },
+    "themeStyle": {
+      "themeFillColor": {
+        "color": "#fff",
+        "opacity": 1
+      },
+      "themeFontColor": {
+        "color": "#616161",
+        "opacity": 1
+      },
+      "themeFontFamily": "Roboto",
+      "themeAccentFillColor": {
+        "color": "#4285F4",
+        "opacity": 1
+      },
+      "themeAccentFontColor": {
+        "color": "#EFEFEF",
+        "opacity": 1
+      },
+      "themeAccentFontFamily": "Roboto",
+      "themeSeriesColor": [
+        {
+          "color": "#4285F4",
+          "opacity": 1
+        },
+        {
+          "color": "#DB4437",
+          "opacity": 1
+        },
+        {
+          "color": "#F4B400",
+          "opacity": 1
+        },
+        {
+          "color": "#0F9D58",
+          "opacity": 1
+        },
+        {
+          "color": "#AB47BC",
+          "opacity": 1
+        },
+        {
+          "color": "#00ACC1",
+          "opacity": 1
+        },
+        {
+          "color": "#FF7043",
+          "opacity": 1
+        },
+        {
+          "color": "#9E9D24",
+          "opacity": 1
+        },
+        {
+          "color": "#5C6BC0",
+          "opacity": 1
+        },
+        {
+          "color": "#F06292",
+          "opacity": 1
+        },
+        {
+          "color": "#00796b",
+          "opacity": 1
+        },
+        {
+          "color": "#c2185b",
+          "opacity": 1
+        },
+        {
+          "color": "#7e57c2",
+          "opacity": 1
+        },
+        {
+          "color": "#03a9f4",
+          "opacity": 1
+        },
+        {
+          "color": "#8bc34a",
+          "opacity": 1
+        },
+        {
+          "color": "#fdd835",
+          "opacity": 1
+        },
+        {
+          "color": "#fb8c00",
+          "opacity": 1
+        },
+        {
+          "color": "#8d6e63",
+          "opacity": 1
+        },
+        {
+          "color": "#9e9e9e",
+          "opacity": 1
+        },
+        {
+          "color": "#607d8b",
+          "opacity": 1
+        }
+      ],
+      "themeIncreaseColor": {
+        "color": "#388e3c",
+        "opacity": 1
+      },
+      "themeDecreaseColor": {
+        "color": "#f44336",
+        "opacity": 1
+      },
+      "themeGridColor": {
+        "color": "#e9e9e9",
+        "opacity": 1
+      }
+    }
+  }
+};
