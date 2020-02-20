17450c68dcd7cdceeed7692589d3fe34b5afd8d0
Yulan Lin <yulanlin@google.com>
2020-02-20 15:00:22 -0600

    made a table

--- table/src/index.js
+++ table/src/index.js
@@ -1,16 +1,53 @@
-const drawViz = (data) => {
-  // print the data to a div
-  if (document.querySelector('div')) {
-    const oldDiv = document.querySelector('div');
-    oldDiv.parentNode.removeChild(oldDiv);
+const clearTable = () => {
+  const table = document.getElementsByTagName("table");
+  if (table.length > 0) {
+    document.body.removeChild(table[0]);
   }
-  const div = document.createElement('div');
-  var msgText = JSON.stringify(data, null, 2);
-  div.innerHTML = `<pre>${msgText}</pre>`;
-  document.body.appendChild(div);
+}
 
- //console.log the data 
- console.log(data);
+const setStyle = (data) => {
+  const fontFamily = data.theme.themeAccentFontFamily;
+  document.body.style.fontFamily = fontFamily;
+  const fontColor = data.theme.themeAccentFontColor.color;
+  document.body.style.color = fontColor;
+}
+
+const drawViz = (data) => {
+  clearTable();
+  setStyle(data);
+
+  const table = document.createElement("table");
+  const rows = data.tables.DEFAULT.rows; 
+
+  // make the header
+  const thead = document.createElement("thead");
+  const thr = document.createElement("tr");
+  for (let heading of data.tables.DEFAULT.headers){
+    const th = document.createElement("th");
+    const text = document.createTextNode(heading.name);
+    th.appendChild(text);
+    thr.appendChild(th);
+  }
+  thead.appendChild(thr);
+  table.append(thead);
+
+  // make the table body
+  const body = document.createElement("tbody");
+  for (let row of rows){
+    const tr = document.createElement("tr");
+    for (let cell of row){
+      const td = document.createElement("td");
+      const text = document.createTextNode(cell);
+      td.appendChild(text)
+      tr.appendChild(td);
+    }
+    body.appendChild(tr);
+  } 
+  table.append(body);
+
+  // append the table to the DOM
+  document.body.appendChild(table);
 };
+
 dscc.subscribeToData(drawViz, {transform: dscc.tableTransform});
 
